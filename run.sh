889c61b7866b9cfb485cd55dd18361d544af2ce2
Jittavat <jittavat.suksumlarn@tesco.com>
2021-10-29 18:16:06 +0700

    install krakend

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ helm repo update
 kubectl apply -f kong/kong.yaml
 helm install kong/kong --set proxy.type=NodePort,proxy.http.nodePort=31500,proxy.tls.nodePort=32500 --generate-name --namespace kong
 
+echo "############# Setup KrakenD ############# "
+kubectl apply -f krakend/krakend.yaml
 
 echo "############# Create test namespace ############# "
 kubectl apply -f app/test-ns.yaml
